0f9ddf77ad556f437fd7148860aecff86815869e
Oliver Davies <oliver@oliverdavies.uk>
2015-12-16 13:39:23 +0000

    Updated publish.sh

--- publish.sh
+++ publish.sh
@@ -1,15 +1,7 @@
 #!/bin/bash
 
-sculpin generate --env=prod
+./vendor/bin/sculpin generate --env=prod --clean --no-interaction
+if [ $? -ne 0 ]; then echo "Could not generate the site"; exit 1; fi
 
-if [ $? -ne 0 ];
-    then echo "Could not generate the site"; exit 1;
-fi
-
-rsync -avze 'ssh -p 22' --delete \
-    output_prod/ \
-    cv.oliverdavies.uk:/var/www/vhosts/cv.oliverdavies.uk/public
-
-if [ $? -ne 0 ];
-    then echo "Could not publish the site"; exit 1;
-fi
+rsync -av --delete output_prod/ deployment@www.oliverdavies.uk:/var/www/vhosts/oliverdavies/cv
+if [ $? -ne 0 ]; then echo "Could not publish the site"; exit 1; fi
